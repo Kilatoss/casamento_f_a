316f5750e2eb3d9bea8c5f533a96cf849cad547c
Afonso Alves Martins <afonso.alves.martins@bee2solutions.com>
2026-03-06 22:22:54 +0000

    The Final Commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -22,17 +22,14 @@ const About = () => {
   return (
     <section id="about" className="intro">
       <div className="intro-content">
-        <span className="section-title">OS NOIVOS</span>
         <h1>Convite de Casamento</h1>
         <p>
-          É como muita alegria que vos convidamos para o nosso casamento, no dia 25 de julho de 2026, em Coimbra. 
-Estamos certos que a vossa presença irá completar este grande dia que tanto ansiamos
-A cerimónia irá realizar-se na capela de S.Miguel da Universidade de Coimbra.
- A cerimónia terá início às 15:00 horas, já a contar com os 15 minutos académicos da noiva.
- O copo d’água e a festa serão realizados no palácio da quinta da Portela, Coimbra.
-        </p>
-      </div>
+          É com muita alegria que vos convidamos para o nosso casamento, no dia 25 de julho de 2026, em Coimbra. 
+Estamos certos que a vossa presença irá completar este grande dia por que tanto ansiamos!
 
+        </p>
+        {/* <img src="img/leafs.png" alt="leafDeco" className="intro-img" /> */}
+      </div>
       <div className="slider-container">
         {images.map((img, index) => (
           <div
